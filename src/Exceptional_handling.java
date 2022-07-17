7815b435d9df5f496abd0acb7e22ccb4c9e32601
mumo274193 <muzammil.mohamed@elektrobit.com>
2022-07-17 22:01:14 +0530

    initial commit

---
+++ src/Exceptional_handling.java
@@ -0,0 +1,12 @@
+public class Exceptional_handling {
+    public static void main(String args[]){
+        try{
+            int x=1;
+            int y=0;
+            int z = x/y;
+        }catch(Exception z)
+        {System.out.println("Divisible by zero");}
+        System.out.println("rest of the code...");
+    }
+}
+
